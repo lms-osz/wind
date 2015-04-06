eb0aae11c0cc0cb1aea876745adc80260f7747e2
DL7JC, Jan <dl7jc@dk0tu.de>
2015-04-06 21:33:20 +0200

    a littebit buggy

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ from tornado.options import define, options
 import os.path
 import json
 import thread
-import sqlite3
+import sys
+import threading
+from threading import Thread
 
+import math
 import config
 import log
-
+import time
 clients = []
 
 class SilentErrorHandler(tornado.web.ErrorHandler):
@@ -77,7 +80,6 @@ def WindDataSender(data):
     for client in clients:
         client.write_message(RealtimeWindDaterFormater(data))
 
-     
 def main():
     # handlers
     handlers = [
@@ -108,7 +110,84 @@ def main():
     except KeyboardInterrupt:
         print ""
         log.info("Server stopped")
- 
+
+hour_wind = open("hour_wind.txt", "w")
+day_wind = open("day_wind.txt", "w")
+year_wind = open("year_wind.txt", "w")
+
+def hour():
+	
+	while 1:	
+
+		x=0
+
+		for x in range (0,59):
+			hour_wind.write("2\n")
+			hour_wind.flush()
+			time.sleep(60)
+			#time.sleep(2)			
+			x+=1
+
+		hour_wind.seek(0)
+		hour_wind.truncate()
+
+def day():
+	
+	while 1:	
+		
+		with open("hour_wind.txt") as f:
+			numbers = []
+			for line in f:
+				numbers.append(int(line))
+			numbers.sort()
+		
+		count = len(numbers)
+		#max_num = max(numbers)
+		#min_num = min(numbers)
+		sum_of_nums = sum(numbers)
+		median1 = numbers[len(numbers)//2]
+		a = str(median1)
+		y = 0
+		
+		for y in range (0,23):
+			day_wind.write("%s\n" % (a))
+			day_wind.flush()
+			time.sleep(3600)
+			#time.sleep(2)
+			y+=1
+
+		hour_wind.seek(0)
+		hour_wind.truncate()
+
+def year():
+
+	while 1:
+
+		with open("day_wind.txt") as g:
+			numbers = []
+
+			for line in g:
+				numbers.append(int(line))
+			numbers.sort()
+
+		count = len(numbers)
+		#max_num = max(numbers)
+		#min_num = min(numbers)
+		sum_of_nums = sum(numbers)
+		median2 = numbers[len(numbers)//2]
+		b = str(median2)		
+		z = 0
+
+		for z in range (0,23):
+			year_wind.write("%s\n" % (b))
+			year_wind.flush()
+			time.sleep(86400)	
+			#time.sleep(2)
+			z+=1
+
 
 if __name__ == "__main__":
-    main()
+	main()
+	Thread(target = hour).start()
+        Thread(target = day).start()
+	Thread(target = year).start()

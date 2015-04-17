e537dd70527fa8a85169e102e674bd9158c2eb6b
jurek <jurek@afrika2.de>
2015-04-17 16:30:04 +0200

    updating config error

--- dataserver/config.py
+++ dataserver/config.py
@@ -5,11 +5,12 @@ CONFIG_PATH = "config.cfg"
 
 # if config does not exits
 if os.path.isfile(CONFIG_PATH) == False:
-    print ("Because no config file exits, a config file will be created")
+    print ("\'" + CONFIG_PATH + "\' is missing, it will be created with default content!")
     file = open(CONFIG_PATH, "w")
     file.write("""[server]\nurl=ws://127.0.0.1/datasocket\n\n[settings]\ndelay=1\npassword=foobar\n""")
     file.close()
     print ("Please check the config in '" + CONFIG_PATH + "'")
+    exit()
 
 
 configParser = ConfigParser.RawConfigParser()

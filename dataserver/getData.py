46c5822977cd65ba625169bdd20dd2a3c7486c0e
Jurek Weber <jurek@afrika2.de>
2015-04-18 22:10:31 +0200

    changing MCP8008.py script

--- dataserver/getData.py
+++ dataserver/getData.py
@@ -14,12 +14,4 @@ GPIO.setup(CS,   GPIO.OUT)
 
 
 def getData(channel):
-    c = mcp3008.getAnalogData(channel, CLK, DIN, DOUT, CS)
-    c = c * 0.00322
-    c = c / 165
-    c = c * 1000
-    c = c - 4
-    c = c * 50
-    c = c / 16
-    c = round(c, 2)
-    return c
+    return mcp3008.getTenBit(channel)
--- dataserver/mcp3008.py
+++ dataserver/mcp3008.py
@@ -1,51 +1,61 @@
-# this is an code example form: http://bit.ly/1FKpK5i
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-HIGH = True  # 3,3V
-LOW  = False # 0V
+# configuring Pins
+pins = {
+    "DIN"   : 24,
+    "DOUT"  : 23,
+    "CLOCK" : 18,
+    "SHDN"  : 25
+}
 
-# SCI function
-def getAnalogData(adCh, CLKPin, DINPin, DOUTPin, CSPin):
-    GPIO.output(CSPin, HIGH)    
-    GPIO.output(CSPin, LOW)
-    GPIO.output(CLKPin, LOW)
-        
-    cmd = adCh
-    cmd |= 0b00011000
+GPIO.setup(pins["CLOCK"], GPIO.OUT)
+GPIO.setup(pins["DOUT"], GPIO.IN)
+GPIO.setup(pins["SHDN"], GPIO.OUT)
+GPIO.setup(pins["DIN"], GPIO.OUT)
 
+def clock():
+    GPIO.output(pins["CLOCK"], True)
+    GPIO.output(pins["CLOCK"], False)
+
+def getTenBit(channel):
+    if ((channel > 7) or (channel < 0)):
+        return -1
+    GPIO.output(pins["SHDN"], True)
+    GPIO.output(pins["SHDN"], False)
+    GPIO.output(pins["CLOCK"], False)
+    
+    command = channel
+    command |= 0x18
+    # config channel
     for i in range(5):
-        if (cmd & 0x10):
-            GPIO.output(DINPin, HIGH)
+        if (command & 0x10):
+            GPIO.output(pins["DIN"], True)
         else:
-            GPIO.output(DINPin, LOW)
-        GPIO.output(CLKPin, HIGH)
-        GPIO.output(CLKPin, LOW)
-        cmd <<= 1
-            
-    adchvalue = 0
+            GPIO.output(pins["DIN"], False)
+        clock();
+        command <<= 1
+    
+    # get data
+    adcout = 0
     for i in range(11):
-        GPIO.output(CLKPin, HIGH)
-        GPIO.output(CLKPin, LOW)
-        adchvalue <<= 1
-        if(GPIO.input(DOUTPin)):
-            adchvalue |= 0x01
-    return adchvalue
+        clock();
+        adcout <<= 1
+        if (GPIO.input(pins["DOUT"])):
+            adcout |= 0x01
+    
+    return adcout;
 
 
-""""CH = 0  # Analog/Digital-Channel
-CLK     = 18 # Clock
-DIN     = 24 # Digital in
-DOUT    = 23  # Digital out
-CS      = 25  # Chip-Select
+if __name__ == "__main__":
+    CHANNEL = 2
+    import time
+    try:
+        while(True):
+            print str(getTenBit(CHANNEL));
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print ""
+        exit()
 
-# Pin-Programmierung
-GPIO.setup(CLK, GPIO.OUT)
-GPIO.setup(DIN, GPIO.OUT)
-GPIO.setup(DOUT, GPIO.IN)
-GPIO.setup(CS,   GPIO.OUT)
-
-while True:
-    print getAnalogData(CH, CLK, DIN, DOUT, CS)
-"""

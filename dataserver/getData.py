3853483c4e8ee253326e4d40826fea7d1b565397
jurek <jurek@afrika2.de>
2015-04-20 21:14:41 +0200

    removing unused stuff

--- dataserver/getData.py
+++ dataserver/getData.py
@@ -1,17 +1,4 @@
-import RPi.GPIO as GPIO
 import mcp3008
-
-CLK     = 18 # Clock
-DIN     = 24 # Digital in
-DOUT    = 23  # Digital out
-CS      = 25  # Chip-Select
-
-# Pin-Programmierung
-GPIO.setup(CLK, GPIO.OUT)
-GPIO.setup(DIN, GPIO.OUT)
-GPIO.setup(DOUT, GPIO.IN)
-GPIO.setup(CS,   GPIO.OUT)
-
 
 def getData(channel):
     return mcp3008.getTenBit(channel)

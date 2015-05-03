f05ae4cf174c89b273c33e5498027c4e80f65775
Jurek Weber <jurek@afrika2.de>
2015-05-03 09:37:10 +0200

    adding one \n at dafault [channels]...

--- dataserver/config.py
+++ dataserver/config.py
@@ -7,7 +7,7 @@ CONFIG_PATH = "config.cfg"
 if os.path.isfile(CONFIG_PATH) == False:
     print ("\'" + CONFIG_PATH + "\' is missing, it will be created with the default content!")
     file = open(CONFIG_PATH, "w")
-    file.write("""[server]\nurl=ws://127.0.0.1/datasocket\n\n[settings]\ndelay=1\ntimes=4\n\npassword=foobar\n\n[channels]wind=\nUbatt=\nIbatt=""")
+    file.write("""[server]\nurl=ws://127.0.0.1/datasocket\n\n[settings]\ndelay=1\ntimes=4\n\npassword=foobar\n\n[channels]\nwind=\nUbatt=\nIbatt=""")
     file.close()
     print ("Please check the config in '" + CONFIG_PATH + "'")
     exit()

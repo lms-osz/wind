2789a61f68712ba9d446cbe5e5a3e844d65bce16
Jurek Weber <jurek@afrika2.de>
2015-05-03 09:45:22 +0200

    try to make this better.....

--- dataserver/app.py
+++ dataserver/app.py
@@ -36,9 +36,9 @@ def on_open(ws):
                 
                 time.sleep((int(config.delay) - 0.0501) / int(config.times))
              
-            wind_tenBit = wind_tenBit / int(config.times);
-            Ubatt_tenBit = Ubatt_tenBit / int(config.times);
-            Ibatt_tenBit = Ibatt_tenBit / int(config.times);
+            wind_tenBit = wind_tenBit / (int(config.times) + 1);
+            Ubatt_tenBit = Ubatt_tenBit / (int(config.times) + 1);
+            Ibatt_tenBit = Ibatt_tenBit / (int(config.times) + 1);
             
             
             ws.send('{"pw":"' + config.password + '", "data":[{"wind":' + str(wind_tenBit) + ',"Ubatt":' + str(Ubatt_tenBit) + ',"Ibatt":' + str(Ibatt_tenBit) + '}]}') # sending data in an json format to the server
